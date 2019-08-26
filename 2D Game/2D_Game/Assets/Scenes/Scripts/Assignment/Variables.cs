683c5a081c295f8deb7fd214ebabe9235726ddd8
Chris <christopher.berry2018@gmail.com>
2019-08-26 17:11:04 -0600

    Added Variables

---
+++ 2D Game/2D_Game/Assets/Scenes/Scripts/Assignment/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string name = "Neon Skull";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(name);
+    }
+
+
+
+}
