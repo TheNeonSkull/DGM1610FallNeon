980b5ab9995007b5df0d8615a98a6ee84074d30b
Chris <christopher.berry2018@gmail.com>
2019-09-04 17:08:24 -0600

    Calc

---
+++ 2D Game/2D_Game/Assets/Scenes/Scripts/Assignment/Functions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    string firstname;
+    string lastname;
+    int age;
+
+int num1;
+int num2;
+
+int total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    //   firstname = "Bob";
+
+    //   lastname = "Neon";
+
+    //   age = 90;
+
+      Customer(firstname, lastname, age);
+      CalcAdd(11,23);
+    }
+    
+    int CalcAdd (int n1, int n2)
+    {
+        Debug.Log("Number 1 =" + n1+"Number 2=" + n2);
+        total = n1 + n2;
+
+        Debug.Log(total);
+        return total;
+      
+
+    }
+
+
+
+    void Customer(string fname, string lname, int a)
+    {
+        Debug.Log(fname + "" + lname + "" + a);
+    }
+    
+}
--- 2D Game/2D_Game/Assets/Scenes/Scripts/Assignment/Hydra.cs
+++ 2D Game/2D_Game/Assets/Scenes/Scripts/Assignment/Hydra.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Variables : MonoBehaviour
+public class Hydra : MonoBehaviour
 {
-    string firstname = "Neon Skull";
-     int age = 20;
-     float height = 5.9f;
-     bool married = true;
-
-
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
 
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
