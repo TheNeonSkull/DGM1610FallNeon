5207e7cdd0c3c202700bc84fb37a75c3f05ac5b4
Chris <christopher.berry2018@gmail.com>
2019-09-16 17:08:19 -0600

    Added Movement/ WASD Buttons/Able to Adjust motion speed

--- CreateWithCode001/Assets/Scripts/PlayerController.cs
+++ CreateWithCode001/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
+
 {
     // Start is called before the first frame update
     void Start()
@@ -11,9 +13,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    public float speed = 5.0f; 
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
     void Update()
     {
         //This Code makes the vehicle move
-        transform.Translate(Vector3.forward*Time.deltaTime*20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward*Time.deltaTime*speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+       
     }
 }
