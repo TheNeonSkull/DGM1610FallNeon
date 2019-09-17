419834ae8e92ff776879aaa6cb2b3f14bd578259
Chris <christopher.berry2018@gmail.com>
2019-09-16 18:24:27 -0600

    Cleaned Up Code

--- CreateWithCode001/Assets/Scripts/PlayerController.cs
+++ CreateWithCode001/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    //Not turining the floats to private so that they can be edited. If I wanted to i could switch all the "publics" to Private.
     public float speed = 5.0f; 
     public float turnSpeed;
     public float horizontalInput;
@@ -20,9 +21,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //This Code makes the vehicle move
+    
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+        //Moves vehicle forward or backwards Using W and S
         transform.Translate(Vector3.forward*Time.deltaTime*speed * forwardInput);
+        //Moves vehicle left or right Using A and D
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
        
     }
