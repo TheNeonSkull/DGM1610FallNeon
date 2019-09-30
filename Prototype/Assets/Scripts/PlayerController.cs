a0a8736365157b755ad1b9682181259cb9fd131a
Chris <christopher.berry2018@gmail.com>
2019-09-30 17:09:30 -0600

    Created Prototype (Feeding Animals). Created boundaries and movement.

---
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Moves the character left and Right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //This keeps the player in bounds on the left side of the screen
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+
+}
