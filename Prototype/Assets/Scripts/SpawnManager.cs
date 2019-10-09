fbf47719b7995a8ee7f2848265332d743802e02a
Chris <christopher.berry2018@gmail.com>
2019-10-09 17:17:42 -0600

    Created Spawn Mgr

---
+++ Prototype/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+            //Random Gen animal index and spawn pos
+        } 
+    }
+            void SpawnRandomAnimal(){ 
+                
+                int animalIndex = Random.Range(0, animalPrefabs.Length);
+                Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
+
+                Instantiate(animalPrefabs[animalIndex], spawnpos, 
+                animalPrefabs[animalIndex].transform.rotation);
+           
+            }
+    }
+
+        
+    
+    
+    
