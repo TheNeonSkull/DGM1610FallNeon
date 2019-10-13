e69beef6f34ca66188501029f48c5949eeec88f9
Chris <christopher.berry2018@gmail.com>
2019-10-13 12:28:06 -0600

    Fixed Spawn Mgr (Stampede) Animals now can be closed and recalled instead of being deleted and unable to be respawned

--- Prototype/Assets/Scripts/SpawnManager.cs
+++ Prototype/Assets/Scripts/SpawnManager.cs
@@ -16,22 +16,13 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-            //Random Gen animal index and spawn pos
-        } 
-    }
-            void SpawnRandomAnimal(){ 
-                
-                int animalIndex = Random.Range(0, animalPrefabs.Length);
-                Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
-
-                Instantiate(animalPrefabs[animalIndex], spawnpos, 
-                animalPrefabs[animalIndex].transform.rotation);
-           
+        if (Input.GetKeyDown(KeyCode.S)){
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);}}
+       
             }
-    }
+    
 
         
     
